6da417ce89b0f9b50fc97eb75edbb6b4814b4bae
Rick Samson <samson.rick@gmail.com>
2020-12-05 08:22:54 +0100

    2020 Day 5 part 1

--- 2020/Days/Day5.cs
+++ 2020/Days/Day5.cs
@@ -9,7 +9,9 @@ namespace AdventOfCode._2020
     class Day5
     {
         private static List<string> Input =>
-                InputHelper.GetInputString(2020, 5).Split(new string[] { " ", Environment.NewLine }, StringSplitOptions.None).ToList();
+                InputHelper.GetInput(2020, 5);
+
+        private int PlaneRows = 127;
 
         public static void Run()
         {
@@ -22,11 +24,63 @@ namespace AdventOfCode._2020
 
         private static int Part1()
         {
-            return 0;
+            Plane plane = new Plane(128, 8);
+            List<int> seatIds = Input.Select(s => plane.CheckSeat(s)).ToList();
+
+            return seatIds.Max();
         }
         private static int Part2()
         {
             return 0;
+        }
+
+        private class Plane
+        {
+            private int RowsOfSeats { get; set; }
+            private int SeatsPerRow { get; set; }
+
+            public Plane(int rows, int seats)
+            {
+                RowsOfSeats = rows;
+                SeatsPerRow = seats;
+            }
+
+            public int CheckSeat(string boardingPass)
+            {
+                int minRow = 0;
+                int maxRow = RowsOfSeats - 1;
+
+                for (int i = 0; i < 7; i++)
+                {
+                    switch (boardingPass[i])
+                    {
+                        case 'F':
+                            maxRow = (int)Math.Floor((minRow + maxRow) / 2.0);
+                            break;
+                        case 'B':
+                            minRow = (int)Math.Floor((minRow + maxRow) / 2.0 + 1);
+                            break;
+                    }
+                }
+
+                int minSeat = 0;
+                int maxSeat = SeatsPerRow - 1;
+
+                for (int i = 7; i < boardingPass.Length; i++)
+                {
+                    switch (boardingPass[i])
+                    {
+                        case 'R':
+                            minSeat = (int)Math.Floor((minSeat + maxSeat) / 2.0 + 1);
+                            break;
+                        case 'L':
+                            maxSeat = (int)Math.Floor((minSeat + maxSeat) / 2.0);
+                            break;
+                    }
+                }
+
+                return minRow * SeatsPerRow + minSeat;
+            }
         }
     }
 }
