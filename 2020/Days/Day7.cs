7ef58d9b1526dba6e4974c03aab28c3c55479119
Rick Samson <samson.rick@gmail.com>
2020-12-07 10:06:44 +0100

    2020 day 7 part 1

--- 2020/Days/Day7.cs
+++ 2020/Days/Day7.cs
@@ -22,12 +22,105 @@ namespace AdventOfCode._2020
 
         private static int Part1()
         {
-            return 0;
+            List<Bag> bags = new List<Bag>();
+
+            foreach (string s in Input)
+            {
+                string[] split = s.Split(new string[] { "bags contain", ",", "." }, StringSplitOptions.RemoveEmptyEntries);
+                int count = split.Length;
+                Bag bag = new Bag(split[0]);
+                List<Bag> subBags = new List<Bag>();
+
+                for (int i = 1; i < count; i++)
+                {
+                    string[] subBagSplit = split[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    if (int.TryParse(subBagSplit[0], out int amount))
+                    {
+                        for (int j = 0; j < amount; j++)
+                            subBags.Add(new Bag(string.Join(' ', subBagSplit[1..])));
+                    }
+
+                }
+
+                bag.AddBags(subBags);
+
+                if (bags.FirstOrDefault(b => b.Color == split[0]) is Bag foundBag)
+                {
+                    foundBag.AddBags(subBags);
+                }
+                else
+                {
+                    bags.Add(bag);
+                }
+
+            }
+
+            List<Bag> CanHoldShinyBag = FindBagsThatCouldContainColor(bags, "shiny gold");
+
+            return CanHoldShinyBag.Select(b => b.Color).Distinct().Count();
         }
 
         private static int Part2()
         {
             return 0;
+        }
+
+        public class Bag : IEquatable<Bag>
+        {
+            private readonly string _Color;
+
+            public string Color
+            {
+                get
+                {
+                    return _Color;
+                }
+            }
+
+            public List<Bag> ContainsBags { get; set; } = new List<Bag>();
+
+            public Bag(string color)
+            {
+                _Color = color.Replace("bags", "").Replace("bag", "").Trim();
+            }
+
+            public void AddBags(List<Bag> bags)
+            {
+                ContainsBags.AddRange(bags);
+            }
+
+            public bool Equals(Bag other)
+            {
+                return Color == other.Color;
+            }
+
+            public bool HasBag(string bagColor)
+            {
+                return ContainsBags.Find(b => b.Color == bagColor) != null;
+            }
+        }
+
+        private static List<Bag> FindBagsThatCouldContainColor(List<Bag> bags, string color)
+        {
+            List<Bag> containsColor = bags.Where(b => b.HasBag(color)).ToList();
+
+            List<Bag> foundBags = new List<Bag>();
+
+            Queue<Bag> queue = new Queue<Bag>(containsColor);
+
+            while (queue.Count > 0)
+            {
+                var bag = queue.Dequeue();
+                foundBags.Add(bag);
+
+                var moreBags = bags.Where(b => b.HasBag(bag.Color));
+
+                foreach (var anotherBag in moreBags)
+                    queue.Enqueue(anotherBag);
+            }
+
+            return foundBags;
         }
     }
 }
