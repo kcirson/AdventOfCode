a74a9363531f3c0a14b5db43d02527ff45334d8c
Rick Samson <samson.rick@gmail.com>
2020-12-08 08:01:09 +0100

    2020 Day 8 part 1

--- 2020/Days/Day8.cs
+++ 2020/Days/Day8.cs
@@ -23,12 +23,86 @@ namespace AdventOfCode._2020
 
         private static int Part1()
         {
-            return 0;
+            Game game = new Game();
+            game.Run(Input);
+
+            return game.GetAccumulator();
         }
 
         private static int Part2()
         {
             return 0;
+        }
+
+        private class Game
+        {
+            private int Accumulator { get; set; }
+            Dictionary<int, string> Instructions { get; set; }
+            List<int> PastInstructions { get; set; }
+
+            public Game()
+            {
+                Accumulator = 0;
+                PastInstructions = new List<int>();
+                Instructions = new Dictionary<int, string>();
+            }
+
+            public void Run(List<string> values)
+            {
+                int count = values.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    Instructions.Add(i, values[i]);
+                }
+
+                bool stop = false;
+                int action = 0;
+
+                while (!stop)
+                {
+                    string value = Instructions[action];
+                    stop = DoAction(value, action, out action);
+                }
+            }
+
+
+            private bool DoAction(string operation, int current, out int next)
+            {
+                bool stop = false;
+                next = 0;
+
+                if (PastInstructions.Contains(current))
+                {
+                    stop = true;
+                    return stop;
+                }
+
+                PastInstructions.Add(current);
+
+                string[] split = operation.Split(' ');
+
+                if (int.TryParse(split[1], out int increase))
+                {
+                    switch (split[0])
+                    {
+                        case "nop":
+                            next = current + 1;
+                            break;
+                        case "acc":
+                            Accumulator += increase;
+                            next = current + 1;
+                            break;
+                        case "jmp":
+                            next = current + increase;
+                            break;
+                    }
+                }
+
+                return stop;
+            }
+
+            public int GetAccumulator() => Accumulator;
+
         }
     }
 }
