675892715f9b56296334e6ed22fc03e4395d8914
Rick Samson <samson.rick@gmail.com>
2020-12-09 00:07:43 +0100

    2019.4

--- 2019/Days/Day4.cs
+++ 2019/Days/Day4.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode._2019
@@ -21,9 +22,10 @@ namespace AdventOfCode._2019
             Console.WriteLine(Part2());
         }
 
-        private static int Part1()
+        public static int Part1()
         {
-            List<int> foundNumber = new List<int>();
+            List<int> CorrectPasswords = new List<int>();
+
             string[] numbers = Input.Split('-');
 
             if (int.TryParse(numbers[0], out int firstNumber))
@@ -32,54 +34,105 @@ namespace AdventOfCode._2019
                 {
                     for (int i = firstNumber; i <= secondNumber; i++)
                     {
-                        bool doubleNumbers = false;
-                        bool increasingNumbers = false;
-                        char[] chars = $"{i}".ToCharArray();
-
-                        int prevNumbers = -1;
-
-                        foreach (char c in chars)
-                        {
-                            if (int.TryParse($"{c}", out int number))
-                            {
-                                if (prevNumbers == -1)
-                                {
-                                    prevNumbers = number;
-                                    continue;
-                                }
-
-                                if (!doubleNumbers)
-                                    doubleNumbers = number == prevNumbers;
-
-                                if (number >= prevNumbers)
-                                {
-                                    prevNumbers = number;
-                                    increasingNumbers = true;
-                                }
-                                else
-                                {
-                                    increasingNumbers = false;
-                                    break;
-                                }
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-
-                        if (doubleNumbers && increasingNumbers)
-                            foundNumber.Add(i);
+                        if (MeetsCriteria(i))
+                            CorrectPasswords.Add(i);
                     }
                 }
             }
 
-            return foundNumber.Count;
+            return CorrectPasswords.Count;
         }
 
-        private static int Part2()
+        public static int Part2()
         {
-            return 0;
+            List<int> CorrectPasswords = new List<int>();
+            List<int> AdvancedPasswords = new List<int>();
+
+            string[] numbers = Input.Split('-');
+
+            if (int.TryParse(numbers[0], out int firstNumber))
+            {
+                if (int.TryParse(numbers[1], out int secondNumber))
+                {
+                    for (int i = firstNumber; i <= secondNumber; i++)
+                    {
+                        if (MeetsCriteria(i))
+                            CorrectPasswords.Add(i);
+                    }
+                }
+            }
+
+            foreach (int simpleCriteria in CorrectPasswords)
+            {
+                if (AdvancedCriteria(simpleCriteria))
+                    AdvancedPasswords.Add(simpleCriteria);
+            }
+
+            return AdvancedPasswords.Count;
+        }
+
+        private static bool MeetsCriteria(int passWord)
+        {
+            bool hasSameNumber = false;
+            bool meets = false;
+            List<int> individual = passWord.ToIntList();
+            int length = individual.Count;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i + 1 < length)
+                {
+                    if (individual[i] == individual[i + 1])
+                        hasSameNumber = true;
+
+                    if (individual[i] <= individual[i + 1])
+                        meets = true;
+                    else
+                    {
+                        meets = false;
+                        break;
+                    }
+
+                }
+            }
+
+            return meets && hasSameNumber;
+        }
+
+        private static bool AdvancedCriteria(int simpleCriteria)
+        {
+            List<int> individual = simpleCriteria.ToIntList();
+            int length = individual.Count - 1;
+
+            List<int> numbersTwice = new List<int>();
+
+            for (int i = 0; i < length; i++)
+            {
+                int current = individual[i];
+
+                if (individual.Count(integer => integer == current) == 2)
+                {
+                    if (!numbersTwice.Contains(i))
+                        numbersTwice.Add(i);
+                }
+            }
+
+            return numbersTwice.Count > 0;
+        }
+
+        private static List<int> ToIntList(this int integer)
+        {
+            List<int> individual = new List<int>();
+
+            while (integer > 0)
+            {
+                individual.Add(integer % 10);
+                integer /= 10;
+            }
+
+            individual.Reverse();
+
+            return individual;
         }
     }
 }
